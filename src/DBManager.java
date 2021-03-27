3c6eedd14efdb8be078dba0d720c73fc2e2c577e
manolin20 <m.bobes@usp.ceu.es>
2021-03-27 11:09:11 +0100

    More changes

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -311,6 +313,9 @@ public void addPrescription(Prescription p) {
 		}
 		
 	}
+	
+	
+	
 	
 	
 
